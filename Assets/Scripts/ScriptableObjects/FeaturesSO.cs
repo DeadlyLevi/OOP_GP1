c8fc9d88724229a32f7988f688d4ba4f9006c44a
Ryssie4499 <sipibea2001@gmail.com>
2023-05-03 17:59:30 +0200

    Inizio Scriptable Objects

---
+++ Assets/Scripts/ScriptableObjects/FeaturesSO.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "", menuName = "")]
+
+    //[ExecuteAlways]           per compilare anche in editor mode
+public class FeaturesSO : ScriptableObject
+{
+    private void Awake()
+    {
+        
+    }
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
