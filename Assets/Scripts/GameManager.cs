8c72ef7dd55ba3e3a4c516a6d4c2b3995aaac6e0
Riccardo Albarello <albarello.riccardo@gmail.com>
2023-05-03 15:58:38 +0200

    SlowTime

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //public GameManager _instance;
+    //public static GameManager Instance { get { return _instance; } }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
